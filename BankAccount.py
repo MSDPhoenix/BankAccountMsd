0dba233b0169a6cdcde032824fb3c42bfe7bd67f
Michael S DuWors <46774296+MSDPhoenix@users.noreply.github.com>
2021-09-21 16:23:12 +0800

    assignment completed

---
+++ BankAccount.py
@@ -0,0 +1,36 @@
+
+# The BankAccount class should have a balance. When a new BankAccount instance is created, if an amount is given, the balance of the account should initially be set to that amount; otherwise, the balance should start at $0. The account should also have an interest rate, saved as a decimal (i.e. 1% would be saved as 0.01), which should be provided upon instantiation. (Hint: when using default values in parameters, the order of parameters matters!)
+
+# The class should also have the following methods:
+
+# deposit(self, amount) - increases the account balance by the given amount
+# withdraw(self, amount) - decreases the account balance by the given amount if there are sufficient funds; if there is not enough money, print a message "Insufficient funds: Charging a $5 fee" and deduct $5
+# display_account_info(self) - print to the console: eg. "Balance: $100"
+# yield_interest(self) - increases the account balance by the current balance * the interest rate (as long as the balance is positive)
+
+class BankAccount:
+    def __init__(self,int_rate = .01,balance = 0):
+        self.int_rate = int_rate
+        self.balance = balance
+    def deposit(self,amount):
+        self.balance += amount
+        return self
+    def withdraw(self,amount):
+        self.balance -= amount
+        if self.balance < amount:
+            self.balance -= 5
+            print("Insufficient funds: Charging a $5 fee")
+        return self
+    def display_account_info(self):
+        print("Balance: ",self.balance)
+        return self
+    def yield_interest(self):
+        if self.balance >0:
+            self.balance = self.balance*(1+self.int_rate)
+        return self 
+
+r2d2 = BankAccount()
+c3p0 = BankAccount()
+
+r2d2.deposit(1000).deposit(1000).deposit(1000).withdraw(700).yield_interest().display_account_info()
+c3p0.deposit(1000).deposit(1000).withdraw(100).withdraw(100).withdraw(100).withdraw(100).yield_interest().display_account_info()
